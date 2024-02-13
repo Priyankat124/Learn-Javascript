83fc43acc54b937832736501554a0b02a983dc7d
Priyankat124 <priyankashri.124@gmail.com>
2024-02-13 22:11:20 +0530

    basic_JS

--- Basic_one/two.js
+++ Basic_one/two.js
@@ -14,3 +14,17 @@ console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
 
 /* prefer not to use var
 because of issue in block scope and functional scope */
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      console.log(`| ${j * i} |`, end = "");
+    }
+    console.log("\n------");
+  }
+  for(let k = 1 ; k <= 10; k++){
+    for(let l = 1; l<=10; l++){
+        console.log(`${k*l}`);
+    }
+    console.log("");
+  }
+  
